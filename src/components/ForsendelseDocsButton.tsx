988c8c8a099b2932588cccd1950608adfb1d79fb
Ugur Alpay Cenar <cenar93@gmail.com>
2023-09-04 20:18:27 +0200

    Egen side for Brukerveiledning (#24)

--- src/components/ForsendelseDocsButton.tsx
+++ src/components/ForsendelseDocsButton.tsx
@@ -1,20 +1,19 @@
-import ForsendelseInfo from "../docs/ForsendelseInfo.mdx";
-import useUserInfoApi from "../hooks/useUserInfoApi";
-import InfoKnapp from "./InfoKnapp";
+import { ExternalLinkIcon } from "@navikt/aksel-icons";
+import { Button } from "@navikt/ds-react";
 
 export default function ForsendelseDocsButton() {
-    const saksbehandlerNavn = useUserInfoApi().hentSaksbehandlerNavn();
     return (
         <div className="agroup fixed bottom-0 right-0 p-2  flex items-end justify-end w-24 h-24">
-            <InfoKnapp
-                className="w-max"
-                roundedIcon={false}
-                buttonText="Brukerveiledning"
-                buttonClassName="border rounded-xl border-solid"
-                title="Forsendelse brukerveiledning"
+            <Button
+                title={"Brukerveiledning"}
+                variant="tertiary"
+                className={`border rounded-xl border-solid`}
+                size="xsmall"
+                icon={<ExternalLinkIcon />}
+                onClick={() => window.open("/forsendelse/brukerveiledning", "_blank")}
             >
-                <ForsendelseInfo saksbehandlerNavn={saksbehandlerNavn} />
-            </InfoKnapp>
+                Brukerveiledning
+            </Button>
         </div>
     );
 }
