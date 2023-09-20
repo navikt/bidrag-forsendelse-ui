0d2273dedc69c6d96aa22ae79c2182829b18bd96
Ugur Alpay Cenar <cenar93@gmail.com>
2023-09-20 10:18:15 +0200

    Støtte for å hente notatmeny basert på behandling detaljer (#31)

--- src/components/dokument/DokumentValgNotat.tsx
+++ src/components/dokument/DokumentValgNotat.tsx
@@ -1,8 +1,10 @@
 import useDokumentApi from "../../hooks/useDokumentApi";
+import { useOpprettForsendelse } from "../../pages/opprettforsendelse/OpprettForsendelseContext";
 import DokumentValgMulti from "./DokumentValgMulti";
 
 export default function DokumentValgNotat() {
-    const { data: dokumentDetaljer, isFetching } = useDokumentApi().hentNotatMalDetaljer();
+    const options = useOpprettForsendelse();
+    const { data: dokumentDetaljer, isFetching } = useDokumentApi().hentNotatMalDetaljer(options);
 
     if (isFetching) {
         return null;
