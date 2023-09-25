e5a9ee41243a5ba3839c46172c2b4c42637865ff
Ugur Alpay Cenar <cenar93@gmail.com>
2023-09-25 14:00:17 +0200

    Lenker til brevmaler (#36)

--- src/components/ForsendelseDocsButton.tsx
+++ src/components/ForsendelseDocsButton.tsx
@@ -3,17 +3,49 @@ import { Button } from "@navikt/ds-react";
 
 export default function ForsendelseDocsButton() {
     return (
-        <div className="agroup fixed bottom-0 right-0 p-2  flex items-end justify-end w-24 h-24">
-            <Button
-                title={"Brukerveiledning"}
-                variant="tertiary"
-                className={`border rounded-xl border-solid`}
-                size="xsmall"
-                icon={<ExternalLinkIcon />}
-                onClick={() => window.open("/forsendelse/brukerveiledning", "_blank")}
-            >
-                Brukerveiledning
-            </Button>
+        <div>
+            <div className="agroup fixed bottom-0 right-0 p-2  flex items-end justify-end w-max h-24 flex flex-row gap-[5px]">
+                <Button
+                    title={"Brevmaler varsler"}
+                    variant="tertiary"
+                    className={`border rounded-xl border-solid w-max`}
+                    size="xsmall"
+                    icon={<ExternalLinkIcon />}
+                    onClick={() =>
+                        window.open(
+                            "https://navno.sharepoint.com/sites/fag-og-ytelser-familie-barnebidrag-og-forskudd/SitePages/Brevmaler-varsler.aspx",
+                            "_blank"
+                        )
+                    }
+                >
+                    Brevmaler varsel
+                </Button>
+                <Button
+                    title={"Brevmaler vedtak"}
+                    variant="tertiary"
+                    className={`border rounded-xl border-solid w-max`}
+                    size="xsmall"
+                    icon={<ExternalLinkIcon />}
+                    onClick={() =>
+                        window.open(
+                            "https://navno.sharepoint.com/sites/fag-og-ytelser-familie-barnebidrag-og-forskudd/SitePages/Brevmaler-vedtak.aspx",
+                            "_blank"
+                        )
+                    }
+                >
+                    Brevmaler vedtak
+                </Button>
+                <Button
+                    title={"Brukerveiledning"}
+                    variant="tertiary"
+                    className={`border rounded-xl border-solid`}
+                    size="xsmall"
+                    icon={<ExternalLinkIcon />}
+                    onClick={() => window.open("/forsendelse/brukerveiledning", "_blank")}
+                >
+                    Brukerveiledning
+                </Button>
+            </div>
         </div>
     );
 }
