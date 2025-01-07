05185191517c94a834b38cd9cee95302f164b59b
Ugur Alpay Cenar <cenar93@gmail.com>
2025-01-07 09:32:45 +0100

    f

--- src/__mocks__/msw/index.ts
+++ src/__mocks__/msw/index.ts
@@ -1,5 +1,6 @@
 export function initMock() {
     if (process.env.NODE_ENV === "development" && process.env.ENABLE_MOCK == "true") {
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
         const { worker } = require("./browser");
         worker
             .start({
