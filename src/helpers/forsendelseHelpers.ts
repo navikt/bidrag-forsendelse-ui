8bc0d8bb9191008fc0fd6ae3ac5a9ba1d7572b40
Ugur Alpay Cenar <cenar93@gmail.com>
2025-06-24 09:55:10 +0200

    Fiks ettersending api

--- src/helpers/forsendelseHelpers.ts
+++ src/helpers/forsendelseHelpers.ts
@@ -23,10 +23,7 @@ export function mapForsendelseResponse(ettersendingsoppgave: Ettersendingsoppgav
     return (prev: AxiosResponse<ForsendelseResponsTo>) => {
         return {
             ...prev,
-            data: {
-                ...prev.data,
-                ettersendingsoppgave: ettersendingsoppgave,
-            },
+            ettersendingsoppgave: ettersendingsoppgave,
         };
     };
 }
